b24120995965988b8f24428bcd9728818c341d5b
Sunil Umasankar <sunil.umasankar1995@gmail.com>
2017-10-17 12:05:21 -0400

    Initial commit

---
+++ a0.hpp
@@ -0,0 +1,14 @@
+/*  Sunil
+ *  UMASANKAR
+ *  suniluma
+ */
+
+#ifndef A0_HPP
+#define A0_HPP
+
+template <typename T, typename Op>
+void omp_scan(int n, const T* in, T* out, Op op) {
+
+} // omp_scan
+
+#endif // A0_HPP
