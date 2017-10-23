ae439260a59c170a78b874f8db8522401d08d2f1
Sunil Umasankar <sunil.umasankar1995@gmail.com>
2017-10-23 15:56:29 -0400

    Clean slate

--- a0.hpp
+++ a0.hpp
@@ -11,20 +11,15 @@ using namespace std;
 template <typename T, typename Op>
 
 void omp_scan(int n, const T* in, T* out, Op op) {
-    int num_threads;
-     omp_set_dynamic( 0 );
-     omp_set_num_threads( omp_get_num_procs() );
+    int thread_id;
+    omp_set_dynamic(false);
+    omp_set_num_threads(omp_get_num_procs());
     #pragma omp parallel
-     { int id=omp_get_thread_num();
-    #pragma omp single
-     num_threads = omp_get_num_threads();
-     cout<<"ID ";
-     cout<<id<<endl;
-     cout<<"NUM Procs ";
-     cout<<num_threads<<endl;
- }
+    {
+        thread_id=omp_get_thread_num();
+        cout<<thread_id;
 
-
+    }
 } // omp_scan
 
 #endif // A0_HPP
