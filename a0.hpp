7571ebc29f4f2578e503372ef3c9463fe4281668
Sunil Umasankar <sunil.umasankar1995@gmail.com>
2017-10-23 15:35:55 -0400

    PRocessors as threads

--- a0.hpp
+++ a0.hpp
@@ -11,28 +11,18 @@ using namespace std;
 template <typename T, typename Op>
 
 void omp_scan(int n, const T* in, T* out, Op op) {
-    static long steps = 100000000;
-    double step = 1.0/(double)steps;
-    double sum = 0.0;
-    omp_set_num_threads(4);
-    #pragma omp parallel for reduction(+:sum)
-    for(int i = 0; i < steps; i++) {
-        double x = (i+0.5)*step;
-        sum += 4.0/(1.0 + x*x);
-    }
-    double pi = step*sum;
-    cout<<pi<<endl;
-
+    int num_threads;
+     omp_set_dynamic( 0 );
+     omp_set_num_threads( omp_get_num_procs() );
     #pragma omp parallel
-    {
-        #pragma omp single
-        {
-            #pragma omp critical
-            cout<<omp_get_thread_num()<<"  ";
-
-            //std::cout<<omp_get_num_threads();
-        }
-    }
+     { int id=omp_get_thread_num();
+    #pragma omp single
+     num_threads = omp_get_num_threads();
+     cout<<"ID ";
+     cout<<id<<endl;
+     cout<<"NUM Procs ";
+     cout<<num_threads<<endl;
+ }
 
 
 } // omp_scan
